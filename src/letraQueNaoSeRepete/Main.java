b4dfd665016dd18fd15131adcfc417c1dc3de23e
Henrique Paoletti Rodrigues <henriquepaolettirodrigues@gmail.com>
2022-02-08 17:59:27 -0300

    Adicionando exercicio 3

--- src/letraQueNaoSeRepete/Main.java
+++ src/letraQueNaoSeRepete/Main.java
@@ -1,7 +1,30 @@
 package letraQueNaoSeRepete;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-
+        ArrayList<String> sequencias= new ArrayList<>();
+        sequencias.add("aaabccccdeeef");
+        sequencias.add("strings");
+        sequencias.add("microsoft visual studio 2022");
+        sequencias.add("abcedoftz");
+        Map<Character,Integer> mapa= new HashMap<>();
+        for(String palavra: sequencias){
+            for (Character letra: palavra.toCharArray()){
+                if(mapa.containsKey(letra)){
+                    mapa.put(letra, mapa.get(letra)+1);
+                } else {
+                    mapa.put(letra,1);
+                }
+            }
+        }
+        for (Map.Entry<Character,Integer> entry: mapa.entrySet()){
+            if(entry.getValue()==1)
+                System.out.println(entry.getKey());
+                break;
+        }
     }
 }
